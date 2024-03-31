2244d6e1b420b8165b92460ed290ac3c3c591c5a
hyejj19@naver.com <hyejj19@naver.com>
2024-03-31 15:42:03 +0900

    [Modify] add page routes

---
+++ src/app/comment/modify/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CommentModify() {
+  return <div>CommentModify page</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import SearchBar from '@/components/SearchBar';
 import PostCard from '@/components/PostCard';
-import Navbar from './components/Navbar';
+import Navbar from '@/components/Navbar';
 
 export default function Home() {
   const handleSearch = (value: string) => {
