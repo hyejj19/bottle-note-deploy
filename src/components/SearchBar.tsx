82e02cd3fc2a3977dbfdaf810ca7870966e9a216
sareum <rkdtkfma@gmail.com>
2024-04-04 20:58:47 +0900

    [Style] add search input styles

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
+import Image from 'next/image';
 
 type Props = {
   handleSearch: (value: string) => void;
@@ -14,18 +15,21 @@ export default function SearchBar({ handleSearch }: Props) {
   };
 
   return (
-    <div className="space-x-1 flex">
+    <div className="space-x-1 flex relative">
       <input
         type="text"
-        className="border w-full"
+        className="w-full bg-bgGray rounded-lg h-10 pl-4 pr-12 outline-none text-main placeholder-main"
         placeholder="어떤 술을 찾고 계신가요?"
         value={searchText}
         onChange={(e) => {
           setSearchText(e.target.value);
         }}
       />
-      <button className="border px-2 w-14" onClick={handleOnClick}>
-        검색
+      <button
+        className="px-2 w-10 absolute top-0 right-1 h-full"
+        onClick={handleOnClick}
+      >
+        <Image src="search.svg" width={80} height={80} alt="search button" />
       </button>
     </div>
   );
