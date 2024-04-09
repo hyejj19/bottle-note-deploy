527ffcf4304a60ccbf90704d095a80963b9ba7c8
sareum <rkdtkfma@gmail.com>
2024-04-09 18:56:22 +0900

    [Modify] add conditional rendering for link and search type

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,12 +2,14 @@
 
 import React, { useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {
+  type?: 'Link' | 'Search';
   handleSearch: (value: string) => void;
 };
 
-export default function SearchBar({ handleSearch }: Props) {
+export default function SearchBar({ type = 'Search', handleSearch }: Props) {
   const [searchText, setSearchText] = useState<string>('');
 
   const handleOnClick = () => {
@@ -16,26 +18,51 @@ export default function SearchBar({ handleSearch }: Props) {
 
   return (
     <div className="relative">
-      <input
-        type="text"
-        className="w-full bg-bgGray rounded-lg h-10 pl-4 pr-12 drop-shadow-[0_3px_3px_rgba(0,0,0,0.30)] outline-none text-main placeholder-main"
-        placeholder="어떤 술을 찾고 계신가요?"
-        value={searchText}
-        onChange={(e) => {
-          setSearchText(e.target.value);
-        }}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            handleOnClick();
-          }
-        }}
-      />
-      <button
-        className="px-2 w-10 absolute top-0 right-1 h-full"
-        onClick={handleOnClick}
-      >
-        <Image src="search.svg" width={80} height={80} alt="search button" />
-      </button>
+      {type === 'Link' ? (
+        <Link href="/search" className="relative">
+          <div className="w-full flex items-center bg-bgGray rounded-lg h-10 pl-4 pr-12 drop-shadow-[0_3px_3px_rgba(0,0,0,0.30)] hover:pointer">
+            <p className="absolute t-0 text-mainGray">
+              어떤 술을 찾고 계신가요?
+            </p>
+            <div className="w-6 absolute right-3 hover:pointer">
+              <Image
+                src="search.svg"
+                width={50}
+                height={50}
+                alt="search button"
+              />
+            </div>
+          </div>
+        </Link>
+      ) : (
+        <>
+          <input
+            type="text"
+            className="w-full bg-bgGray rounded-lg h-10 pl-4 pr-12 drop-shadow-[0_3px_3px_rgba(0,0,0,0.30)] outline-none text-mainCoral placeholder-mainGray"
+            placeholder="어떤 술을 찾고 계신가요?"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleOnClick();
+              }
+            }}
+          />
+          <button
+            className="px-2 w-10 absolute top-0 right-1 h-full"
+            onClick={handleOnClick}
+          >
+            <Image
+              src="search.svg"
+              width={80}
+              height={80}
+              alt="search button"
+            />
+          </button>
+        </>
+      )}
     </div>
   );
 }
