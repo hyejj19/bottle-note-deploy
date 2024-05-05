0557e6108e852c4bd9bf06b02baae90713d862f5
sareum <rkdtkfma@gmail.com>
2024-05-05 22:44:22 +0900

    [Modify] Apply category menu as constants

--- src/app/(primary)/_components/CategoryList.tsx
+++ src/app/(primary)/_components/CategoryList.tsx
@@ -1,53 +1,31 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import { LinkData } from '@/types/LinkButton';
+import { CATEGORY_MENUS } from '@/constants/common';
 import LinkButton from '@/components/LinkButton';
 import whiskeyImg from 'public/category_whisky1.png';
 
 function CategoryList() {
-  const linkData: LinkData[] = [
-    {
-      engName: 'Single malt',
-      korName: '싱글몰트',
-      listType: 'Half',
-      linkSrc: '/search/singleMalt',
-      imgSrc: whiskeyImg,
-    },
-    {
-      engName: 'Blended malt',
-      korName: '블렌디드 몰트',
-      listType: 'Half',
-      linkSrc: '/search/singleMalt',
-      imgSrc: whiskeyImg,
-    },
-    {
-      engName: 'Blended',
-      korName: '블렌디드',
-      listType: 'Half',
-      linkSrc: '/search/singleMalt',
-      imgSrc: whiskeyImg,
-    },
-    {
-      engName: 'America(Bourbon)',
-      korName: '아메리카(버번)',
-      listType: 'Half',
-      linkSrc: '/search/singleMalt',
-      imgSrc: whiskeyImg,
-    },
-    {
-      engName: 'Rye',
-      korName: '라이',
-      listType: 'Half',
-      linkSrc: '/search/singleMalt',
-      imgSrc: whiskeyImg,
-    },
-    {
-      engName: 'Other',
-      korName: '기타',
-      listType: 'Half',
-      linkSrc: '/search/singleMalt',
-      imgSrc: whiskeyImg,
-    },
-  ];
+  const [menu, setMenu] = useState<[] | LinkData[]>([]);
+
+  useEffect(() => {
+    const categories = Object.values(CATEGORY_MENUS).filter(
+      (category) => category !== CATEGORY_MENUS.All,
+    );
+
+    const filterCategory: LinkData[] = categories.map((category) => {
+      return {
+        engName: category.eng,
+        korName: category.kor,
+        listType: 'Half',
+        linkSrc: `/search/${category.link}`,
+        imgSrc: whiskeyImg,
+      };
+    });
+    setMenu(filterCategory);
+  }, []);
+
   return (
     <div className="space-y-3">
       <LinkButton
@@ -58,7 +36,7 @@ function CategoryList() {
         }}
       />
       <div className="grid grid-cols-2 gap-3">
-        {linkData.map((data) => (
+        {menu.map((data) => (
           <LinkButton key={data.engName} data={data} />
         ))}
       </div>
---
+++ src/constants/common.ts
@@ -0,0 +1,13 @@
+export const CATEGORY_MENUS = {
+  All: { kor: '전체', eng: 'All', link: 'all' },
+  SingleMalt: { kor: '싱글몰트', eng: 'Single malt', link: 'SingleMalt' },
+  BlendedMalt: {
+    kor: '블렌디드 몰트',
+    eng: 'Blended malt',
+    link: 'blendedMalt',
+  },
+  Blended: { kor: '블렌디드', eng: 'Blended', link: 'blended' },
+  America: { kor: '아메리카(버번)', eng: 'America(Bourbon)', link: 'america' },
+  Rye: { kor: '라이', eng: 'Rye', link: 'rye' },
+  Other: { kor: '기타', eng: 'Other', link: 'other' },
+};
