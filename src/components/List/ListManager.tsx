de5be8e8323836f954b5be212ea53eeb4e56d05a
hyejj19@naver.com <hyejj19@naver.com>
2024-05-20 22:32:25 +0900

    [Style] 옵션 dropdown ui 추가

--- src/components/List/ListManager.tsx
+++ src/components/List/ListManager.tsx
@@ -3,6 +3,9 @@
 import Image from 'next/image';
 import ArrowDownIcon from 'public/Arrow-Down_Sub_Coral.svg';
 import DescendingIcon from 'public/Descending_Sub_Coral.svg';
+import BackDrop from '../BackDrop';
+import OptionDropdown from '../OptionDropdown';
+import { useState } from 'react';
 
 interface Props {
   total: number;
@@ -18,32 +21,41 @@ const ListManager = ({
   filterOptions,
   hanldeSortOption,
 }: Props) => {
-  return (
-    <article className="flex justify-between items-center text-mainGray text-sm pb-3.25 border-mainBlack border-b">
-      <span className="text-xs text-mainGray">{`총 ${total}개`}</span>
-      <div className="flex gap-1.5 ">
-        <button>
-          <Image src={DescendingIcon} alt="내림차순" />
-        </button>
-        <button
-          className="label-default flex items-center gap-1 px-2.5 py-1 rounded-md text-xxs"
-          onClick={() => alert('하단 bottom sheet 으로 옵션 표시')}
-        >
-          <span>{sortOptions[0]}</span>
-          <Image src={ArrowDownIcon} alt="정렬" />
-        </button>
+  const [isOptonShow, setIsOptionShow] = useState(false);
+  const handleOptionsShow = () => {
+    setIsOptionShow((prev) => !prev);
+  };
 
-        {filterOptions && (
+  return (
+    <>
+      <article className="flex justify-between items-center text-mainGray text-sm pb-3.25 border-mainBlack border-b">
+        <span className="text-xs text-mainGray">{`총 ${total}개`}</span>
+        <div className="flex gap-1.5 ">
+          <button>
+            <Image src={DescendingIcon} alt="내림차순" />
+          </button>
           <button
             className="label-default flex items-center gap-1 px-2.5 py-1 rounded-md text-xxs"
-            onClick={() => alert('하단 bottom sheet 으로 옵션 표시')}
+            onClick={handleOptionsShow}
           >
-            <span>{filterOptions[0]}</span>
-            <Image src={ArrowDownIcon} alt="필터" />
+            <span>{sortOptions[0]}</span>
+            <Image src={ArrowDownIcon} alt="정렬" />
           </button>
-        )}
-      </div>
-    </article>
+
+          {filterOptions && (
+            <button
+              className="label-default flex items-center gap-1 px-2.5 py-1 rounded-md text-xxs"
+              onClick={handleOptionsShow}
+            >
+              <span>{filterOptions[0]}</span>
+              <Image src={ArrowDownIcon} alt="필터" />
+            </button>
+          )}
+        </div>
+      </article>
+
+      {isOptonShow && <OptionDropdown handleClose={handleOptionsShow} />}
+    </>
   );
 };
 
---
+++ src/components/OptionDropdown.tsx
@@ -0,0 +1,33 @@
+import BackDrop from './BackDrop';
+
+interface Props {
+  handleClose: () => void;
+}
+
+export default function OptionDropdown({ handleClose }: Props) {
+  const MOCK_OPTIONS = ['인기도순', '별점순', '찜하기순', '댓글순'];
+  const type = '정렬';
+
+  return (
+    <BackDrop isShow>
+      <div className="w-full h-full flex flex-col justify-end items-center px-4 gap-3 pb-2">
+        <section className="w-full bg-white rounded-xl divide-y">
+          <article className="py-4 text-center text-mainGray text-sm">
+            {type}
+          </article>
+          {MOCK_OPTIONS.map((option) => (
+            <article key={option} className="py-4 text-center text-subCoral">
+              {option}
+            </article>
+          ))}
+        </section>
+        <button
+          className="w-full bg-white rounded-xl py-4"
+          onClick={handleClose}
+        >
+          닫기
+        </button>
+      </div>
+    </BackDrop>
+  );
+}
