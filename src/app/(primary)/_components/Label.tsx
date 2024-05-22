3b349a12c531c19f354dc350ac6df09d2685edf3
sareum <rkdtkfma@gmail.com>
2024-05-22 22:42:14 +0900

    [Style] Design details modification

--- src/app/(primary)/_components/Label.tsx
+++ src/app/(primary)/_components/Label.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import Image from 'next/image';
 
 interface Props {
   name: string;
   style?: string;
+  icon?: string;
 }
 
-function Label({ name, style = 'border-white' }: Props) {
+function Label({
+  name,
+  icon,
+  style = 'border-white px-2.5 py-1 rounded-md',
+}: Props) {
   return (
     <div>
-      <div
-        className={`px-2.5 py-1 border rounded-md text-xxs inline-block ${style}`}
-      >
-        {name}
+      <div className={`border text-xxs inline-block ${style}`}>
+        <div className="flex items-center">
+          {icon && (
+            <Image
+              className="mr-1"
+              src={icon}
+              width={14}
+              height={14}
+              alt="best"
+            />
+          )}
+          {name}
+        </div>
       </div>
     </div>
   );
