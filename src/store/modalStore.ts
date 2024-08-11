f38fbc41f15cd3909aef8f82182457718238c58c
Hyejung Park <hyejj19@naver.com>
2024-08-11 15:42:25 +0900

    [#116] 별점 요청 API 연동 (#140)
    * [Feat] 별점 페이지 > 별점 api 연동
    
    * [Feat] 로그인 모달창 연동
    
    * [Refactor] global login modal
    
    * [Chore] 리뷰 반영

--- src/store/modalStore.ts
+++ src/store/modalStore.ts
@@ -2,12 +2,17 @@ import { create } from 'zustand';
 
 interface ModalStore {
   isShowModal: boolean;
+  isShowLoginModal: boolean;
   handleModal: () => void;
+  handleLoginModal: () => void;
 }
 
 const useModalStore = create<ModalStore>((set) => ({
   isShowModal: false,
+  isShowLoginModal: false,
   handleModal: () => set((state) => ({ isShowModal: !state.isShowModal })),
+  handleLoginModal: () =>
+    set((state) => ({ isShowLoginModal: !state.isShowLoginModal })),
 }));
 
 export default useModalStore;
